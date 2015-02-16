6ca667b2969cba66433098f9633e92b965edd2ed
Clem Freeman and Joey Sandine <clemfree@gmail.com>
2015-02-16 11:00:06 -0800

    Add styling for contacts

--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,16 @@ $("form#new-contact").submit(function(event) {
 
   var newContact = { firstName: firstName, lastName: lastName, address: address };
 
-  $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " "
+  $("ul#contacts").append("<li class='list-group-item'><span class='contact'>" + newContact.firstName + " "
     + newContact.lastName + "</span></li>");
 
   $("li#first-name").append(newContact.firstName);
   $("li#last-name").append(newContact.lastName);
   $("li#address").append(newContact.address);
 
-// $(".contact").last().click(function())
-  $("#show-contact").show("slow");
+  $(".contact").last().click(function() {
+    $("#show-contact").show("slow");
+  });
+
   event.preventDefault();
 });
