f55fc5b88fabc4cf16ad4a12c1bd36f08adbece1
Clem Freeman and Joey Sandine <jasandine@gmail.com>
2015-02-19 11:00:11 -0800

    Fix typo

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $("#show-contact").hide();
 
 $("form#new-contact").submit(function(event) {
   var firstName = $("input#new-first-name").val(),
-      lastName = $("input#new-last-name").val(),
+      lastName = $("input#new-last-name").val();
 
   var newContact = { firstName: firstName, lastName: lastName, addresses: [] };
 
@@ -18,11 +18,6 @@ $("form#new-contact").submit(function(event) {
     newContact.addresses.push(newAddress);
   });
 
-  $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>")
-
-
-
-
   $(".contact").last().click(function() {
     $("#show-contact").show("slow");
 
@@ -32,7 +27,7 @@ $("form#new-contact").submit(function(event) {
 
     $("ul#addresses").text("");
     newContact.forEach(function(address) {
-      ("ul#addresses").append("<li>" + address.street + address.city + address.state + "</li>");
+      ("ul#addresses").append("<li class='list-group-item'>" + address.street + address.city + address.state + "</li>");
     });
   });
 
