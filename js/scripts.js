4068b6bf3fe496df205292425997d8e347f15290
Clem Freeman and Joey Sandine <jasandine@gmail.com>
2015-02-19 10:48:24 -0800

    Redesign scripts to handle multiple addresses

--- js/scripts.js
+++ js/scripts.js
@@ -3,20 +3,39 @@ $("#show-contact").hide();
 $("form#new-contact").submit(function(event) {
   var firstName = $("input#new-first-name").val(),
       lastName = $("input#new-last-name").val(),
-      address = $("input#new-address").val();
 
-  var newContact = { firstName: firstName, lastName: lastName, address: address };
+  var newContact = { firstName: firstName, lastName: lastName, addresses: [] };
 
   $("ul#contacts").append("<li class='list-group-item'><span class='contact'>" + newContact.firstName + " "
     + newContact.lastName + "</span></li>");
 
-  $("li#first-name").text("First Name: " + newContact.firstName);
-  $("li#last-name").text("Last Name: " + newContact.lastName);
-  $("li#address").text("Address: " + newContact.address);
+  $(".new-address").each(function() {
+    var streetInput = $(this).find("input.new-street").val();
+    var cityInput = $(this).find("input.new-city").val();
+    var stateInput = $(this).find("input.new-state").val();
+
+    var newAddress = { street: streetInput, city: cityInput, state: stateInput };
+    newContact.addresses.push(newAddress);
+  });
+
+  $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>")
+
+
+
 
   $(".contact").last().click(function() {
     $("#show-contact").show("slow");
+
+    $("#show-contact h2").text(newContact.firstName);
+    $("li#first-name").text("First Name: " + newContact.firstName);
+    $("li#last-name").text("Last Name: " + newContact.lastName);
+
+    $("ul#addresses").text("");
+    newContact.forEach(function(address) {
+      ("ul#addresses").append("<li>" + address.street + address.city + address.state + "</li>");
+    });
   });
+
 
   $("#new-contact").trigger("reset");
 
